b6d14c089aef72b3c390627d716a07585f5fde50
Malav Solanki <malav23306@iiitd.ac.in>
2024-03-07 14:37:36 +0530

    Value Updation Bug fix and Subtraction Function
    This Commit to repository aims to fix some to fix bugs related to values which were not previously updating in register after each operation along with an addition in features with "Subtraction".

--- trial.py
+++ trial.py
@@ -1,13 +1,21 @@
 # Register Set with DEFAULT VALUES
 reg1 = ["0000",0]
-reg2 = ["0001",0]
-reg3 = ["0010",0]
+reg2 = ["0001",2]
+reg3 = ["0010",9]
+
 
 def add(rd,rs1,rs2):
     opcode = "0110011"
     funct7 = "0000000"
     funct3 = "000"
     rd[1] = rs1[1]+rs2[1]
+    return funct7+rs2[0]+rs1[0]+funct3+rd[0]+opcode
+
+def sub(rd,rs1,rs2):
+    opcode = "0110011"
+    funct7 = "0100000"
+    funct3 = "000"
+    rd[1] = rs1[1]-rs2[1]
     return funct7+rs2[0]+rs1[0]+funct3+rd[0]+opcode
 
 
@@ -21,49 +29,54 @@ with open("coprj_mvy\input.txt") as f:
         registers = x[1].split(",")
         reglist = []
         for i in registers:
-            reglist.append(int(i[-1]))
+            reglist.append(int(i[-1])-1)
         # print(reglist)
         regcount = 0
         for j in reglist:
             if regcount == 0:
-                if j+1==1:
+                if j==0:
                     rd = reg1
                     regcount+=1
-                elif j+1==2:
+                elif j==1:
                     rd = reg2
                     regcount+=1
-                elif j+1==3:
+                elif j==2:
                     rd = reg3
                     regcount+=1
             elif regcount == 1:
-                if j+1==1:
+                if j==0:
                     rs1 = reg1
                     regcount+=1
-                elif j+1==2:
+                elif j==1:
                     rs1 = reg2
                     regcount+=1
-                elif j+1==3:
+                elif j==2:
                     rs1 = reg3
                     regcount+=1
             elif regcount == 2:
-                if j+1==1:
+                if j==0:
                     rs2 = reg1
                     regcount+=1
-                elif j+1==2:
+                elif j==1:
                     rs2 = reg2
                     regcount+=1
-                elif j+1==3:
+                elif j==2:
                     rs2 = reg3
                     regcount+=1
+        # print(rd,rs1,rs2)
         if command=="add":
             bincode.append(add(rd,rs1,rs2))
-            
+        elif command=="sub":
+            bincode.append(sub(rd,rs1,rs2))
 with open("coprj_mvy\output.txt","w+") as g:
     for i in bincode:
             g.write(i+"\n")
             
-print("Regiser 1 =",reg1)
-print("Regiser 2 =",reg2)
-print("Regiser 3 =",reg3)
+            
+
+print("Updated Register values")
+print("Register 1 =",reg1[1])
+print("Register 2 =",reg2[1])
+print("Register 3 =",reg3[1])
 
 # print(x)
