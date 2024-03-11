8496b192041400e489d37963cedb5616c3720f2e
VyomSitapara <162605846+VyomSitapara@users.noreply.github.com>
2024-03-11 15:03:59 +0530

    Added or function
    Signed-off-by: VyomSitapara <162605846+VyomSitapara@users.noreply.github.com>

--- main.py
+++ main.py
@@ -56,7 +56,11 @@ def xor_bitwise(rd,rs1,rs2):
     funct7 = "0000000"
     funct3 = "100"
     return funct7+rs2[0]+rs1[0]+funct3+rd[0]+opcode
-
+def or_bitwise(rd,rs1,rs2):
+    opcode="0110011"
+    funct7="0000000"
+    funct3="110"
+    return funct7+rs2[0]+rs1[0]+funct3+rd[0]+opcode
 def srl(rd,rs1,rs2):
     opcode = "0110011"
     funct7 = "0000000"
@@ -66,7 +70,7 @@ def srl(rd,rs1,rs2):
 def and_bitwise(rd,rs1,rs2):
     opcode = "0110011"
     funct7 = "0000000"
-    funct3 = "110"
+    funct3 = "111"
     return funct7+rs2[0]+rs1[0]+funct3+rd[0]+opcode
 
 def sw(imm,rs1,rs2):
